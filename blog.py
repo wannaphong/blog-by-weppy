049d77bb7ea1ad003366a0af7bc6799945948cb2
wannaphongcom <wannaphong@yahoo.com>
2016-04-08 12:52:15 +0700

    add code pip install

--- blog.py
+++ blog.py
@@ -1,4 +1,15 @@
-from weppy import App
+import pip
+try:
+    from weppy import App
+    from pydblite import Base
+except:
+    pip.main(['install','weppy'])
+    pip.main(['install','pydblite'])
+    from weppy import App
+    from pydblite import Base
+
+db = Base('test.pydb') # สร้างไฟล์ฐานข้อมูล test.pydb
+db.create('name', 'age', 'size') # สร้าง field เก็บข้อมูล
 app = App(__name__)
 
 @app.route("/")
